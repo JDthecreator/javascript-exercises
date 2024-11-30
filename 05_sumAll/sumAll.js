5df92cdb16db188b91571f987331612170b61853
JDthecreator <jetlyfejae@gmail.com>
2024-11-30 07:05:01 -0500

    Completed Lesson 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  result = 0;
 
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+
+  if (a < b) {
+    for (i = a; i <= b; i++) {
+      result += Number([i]);
+    }
+  }
+
+  if (a > b) {
+    for (i = a; i >= b; i--) {
+      result += Number([i]);
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
